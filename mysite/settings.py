6da5f8e5175cf1a1caf006cd074862ca1c7056c0
N3CROM4NC3R <santi16648@gmail.com>
2022-06-12 15:44:01 -0400

    fix: adding the heroku site to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0","localhost"]
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0","localhost", "https://mysterious-oasis-89780.herokuapp.com/"]
 
 
 # Application definition
