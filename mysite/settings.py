ec2933195fd82c5321e05a013fab488083ac8d4f
N3CROM4NC3R <santi16648@gmail.com>
2022-06-12 14:56:58 -0400

    fix: pystache version upgrade

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
 import os
 from pathlib import Path
 
@@ -17,12 +18,22 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Env config
+# Declaring the env object
+env = environ.Env()
+# 
+environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-8nrimvlw8z*4su3)9m56cwla-6z$qj-kwed6qw#cga+00kh1ee'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
